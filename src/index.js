846140afd7be8c1b71bd2837f6d742d8a65497a9
najeesimmons <najee.bryant.simmons@gmail.com>
2022-09-29 15:54:29 -0400

    added AuthContextProvider to index.js

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import "./index.scss";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import { PostsContextProvider } from "./context/PostsContext";
+import { AuthContextProvider } from "./context/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Router>
     <React.StrictMode>
+      <AuthContextProvider>
       <PostsContextProvider>
         <App />
       </PostsContextProvider>
+      </AuthContextProvider>
     </React.StrictMode>
   </Router>
 );
