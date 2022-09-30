37f7a4515714bac665b148d144be12e17da860fb
najeesimmons <najee.bryant.simmons@gmail.com>
2022-09-30 14:50:59 -0400

    added logout button to nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,7 +1,12 @@
 import { Link } from "react-router-dom";
 import styles from "./Nav.module.scss";
+import { useLogout } from "../../hooks/useLogout";
 
 const Nav = () => {
+  const { logout } = useLogout();
+  const handleClick = () => {
+    logout();
+  };
   return (
     <div className={styles.div}>
       <nav className={styles.nav}>
@@ -9,9 +14,14 @@ const Nav = () => {
           <p className={styles.stack}>stack</p>
           <p className={styles.underflow}>underflow</p>
         </Link>
+        <div>
+          <button onClick={handleClick}>Log out</button>
+        </div>
         <div className={styles.auth}>
           <Link to="/login">Log in</Link>
-          <Link to="/signup" className={styles.signup}>Sign up</Link>
+          <Link to="/signup" className={styles.signup}>
+            Sign up
+          </Link>
         </div>
       </nav>
     </div>
