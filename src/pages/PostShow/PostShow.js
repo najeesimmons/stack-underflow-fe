6b732c8954ae147e5fdbc288bcd8f4a761b1278a
najeesimmons <najee.bryant.simmons@gmail.com>
2022-10-08 15:45:37 -0400

    added function to show edit form/ added conditional rendering for edit form, added onclick to control state -- PostShow

--- src/pages/PostShow/PostShow.js
+++ src/pages/PostShow/PostShow.js
@@ -1,5 +1,5 @@
 //from react
-import { useState } from "react";
+import { useState, useEffect } from "react";
 //react-router-dom
 import { useParams, useNavigate } from "react-router-dom";
 //custom hooks
@@ -17,7 +17,13 @@ const PostShow = ({ URL }) => {
   // rendering issues when there is no logged in user
   // const isUser = user._id === post.user_id;
   const [editForm, setEditForm] = useState(post);
+  const [isEditing, setIsEditing] = useState(false);
+
   const navigate = useNavigate();
+
+  useEffect(() => {
+    document.body.style.background = "white";
+  }, []);
 
   const handleChange = (event) => {
     setEditForm({ ...editForm, [event.target.name]: event.target.value });
@@ -52,6 +58,29 @@ const PostShow = ({ URL }) => {
     navigate("/");
   };
 
+  const handleEdit = async () => {
+    if (!user) {
+      // throw Error
+      console.log("You must be logged in to delete a post.");
+      return;
+    }
+
+    if (!user._id === post.user_id) {
+      //throw Error
+      console.log("You're not authorized to delete this post.");
+      return;
+    }
+
+    const response = await fetch(`${URL}posts/${id}`, {
+      method: "PUT",
+      headers: {
+        Authorization: `Bearer ${user.token}`,
+      },
+    });
+
+    const data = await response.json();
+  };
+
   return (
     <section className="post-show-container">
       <h2>{post.title}</h2>
@@ -62,27 +91,34 @@ const PostShow = ({ URL }) => {
       <div dangerouslySetInnerHTML={{ __html: post.body }} />
       <p>{post.user_id}</p>
       {user._id === post.user_id && (
-        <button id="delete" onClick={handleDelete} className="button">
-          Delete
-        </button>
+        <>
+          <button id="delete" onClick={handleDelete} className="button">
+            Delete
+          </button>
+          <button id="edit" onClick={()=> setIsEditing(!isEditing)} className="button">
+            Edit
+          </button>
+        </>
       )}
-      <form>
-        {/* onSubmit={handleSubmit} */}
-        <input
-          type="text"
-          value={editForm.title}
-          name="title"
-          placeholder="title"
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          value={editForm.body}
-          name="body"
-          placeholder="body"
-          onChange={handleChange}
-        />
-      </form>
+      {isEditing && (
+        <form>
+          {/* onSubmit={handleSubmit} */}
+          <input
+            type="text"
+            value={editForm.title}
+            name="title"
+            placeholder="title"
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            value={editForm.body}
+            name="body"
+            placeholder="body"
+            onChange={handleChange}
+          />
+        </form>
+      )}
     </section>
   );
 };
